ad39707cf8e1c77e37501aff30b443f045e09f7e
albertrwaldron <63806082+albertrwaldron@users.noreply.github.com>
2023-01-25 13:27:03 -0500

    Create setup.sh

---
+++ setup.sh
@@ -0,0 +1,13 @@
+mkdir tester_fw
+mkdir -p resource/asnlib/publicdata/encrypted
+mkdir resource/asnlib/public
+wget -q https://raw.githubusercontent.com/gt-cse-6040/skills_oh_week_05/main/public.txt
+wget -q https://raw.githubusercontent.com/gt-cse-6040/skills_oh_week_05/main/publicdata.txt
+wget -q https://raw.githubusercontent.com/gt-cse-6040/skills_oh_week_05/main/publicdata_encrypted.txt
+wget -q https://raw.githubusercontent.com/gt-cse-6040/skills_oh_week_05/main/tester_fw.txt
+wget -P tester_fw/ -i tester_fw.txt -q
+wget -P resource/asnlib/public/ -i public.txt -q
+wget -P resource/asnlib/publicdata/ -i publicdata.txt -q
+wget -P resource/asnlib/publicdata/encrypted -i publicdata_encrypted.txt -q
+pip install cryptography
+rm public.txt publicdata.txt publicdata_encrypted.txt tester_fw.txt
